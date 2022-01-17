01604ebe31c15cc2a0a25fa8297fd4c7d01cb4fd
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2022-01-17 09:21:32 -0300

    Create github.py

---
+++ github.py
@@ -0,0 +1,63 @@
+import python
+import os 
+import webbrosser
+
+
+print ('''
+
+ _____  _____  _____  _   _  _   _ ______ 
+|  __ \|_   _||_   _|| | | || | | || ___ \
+| |  \/  | |    | |  | |_| || | | || |_/ /
+| | __   | |    | |  |  _  || | | || ___ \
+| |_\ \ _| |_   | |  | | | || |_| || |_/ /
+ \____/ \___/   \_/  \_| |_/ \___/ \____/ 
+                                          
+
+''')
+
+    headers = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36')]
+                               
+   
+   
+Request URL: https://github.com/login
+Request Method: GET
+Status Code: 200 
+Remote Address: 20.201.28.151:443
+Referrer Policy: strict-origin-when-cross-origin
+cache-control: no-store
+content-encoding: gzip
+content-security-policy: default-src 'none'; base-uri 'self'; block-all-mixed-content; child-src github.com/assets-cdn/worker/ gist.github.com/assets-cdn/worker/; connect-src 'self' uploads.github.com objects-origin.githubusercontent.com www.githubstatus.com collector.githubapp.com api.github.com github-cloud.s3.amazonaws.com github-production-repository-file-5c1aeb.s3.amazonaws.com github-production-upload-manifest-file-7fdce7.s3.amazonaws.com github-production-user-asset-6210df.s3.amazonaws.com cdn.optimizely.com logx.optimizely.com/v1/events translator.github.com wss://alive.github.com; font-src github.githubassets.com; form-action 'self' github.com gist.github.com objects-origin.githubusercontent.com; frame-ancestors 'none'; frame-src render.githubusercontent.com viewscreen.githubusercontent.com notebooks.githubusercontent.com; img-src 'self' data: github.githubassets.com identicons.github.com collector.githubapp.com github-cloud.s3.amazonaws.com secured-user-images.githubusercontent.com/ *.githubusercontent.com; manifest-src 'self'; media-src github.com user-images.githubusercontent.com/; script-src github.githubassets.com; style-src 'unsafe-inline' github.githubassets.com; worker-src github.com/assets-cdn/worker/ gist.github.com/assets-cdn/worker/
+content-type: text/html; charset=utf-8
+etag: W/"8e7283836d0839f56148936ce9350874"
+expect-ct: max-age=2592000, report-uri="https://api.github.com/_private/browser/errors"
+permissions-policy: interest-cohort=()
+referrer-policy: origin-when-cross-origin, strict-origin-when-cross-origin
+server: GitHub.com
+set-cookie: _gh_sess=IbtwoiRz2YOzsDVA3Ghx1xLX91eUZP%2B5nVzyRwQuQvzc%2ByzyEfwMAuN0xoMrYqNqTZe%2FApN7VHAiCySkeE3aKIC5fnM7U%2FSyDc1oB8EnWjT09ZMor91iOOnksFLFD3Pv7olnQqvo1GMigBV4W0LmTZ5YDFOfog88Ni6bOss1dR0%2FJloYMJtQYByXIAx8DXn2LhYOG2mjRAObDc43EpNh9Rg5KrW42dfBeKBzcn5xtvI6rzDDW9SPLygFwLOLMHIlRjU6x85bP1R3vHs7cWVPvA%3D%3D--CtCAPqUnqV1%2B7H8O--uuSXHvbtOsjL7FjBccG6TQ%3D%3D; path=/; secure; HttpOnly; SameSite=Lax
+strict-transport-security: max-age=31536000; includeSubdomains; preload
+vary: X-PJAX, X-PJAX-Container
+vary: Accept-Encoding, Accept, X-Requested-With
+x-content-type-options: nosniff
+x-frame-options: deny
+x-github-request-id: 1B19:7295:A8B5D7:F96BD8:61E55A43
+x-xss-protection: 0
+:authority: github.com
+:method: GET
+:path: /login
+:scheme: https
+accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+accept-encoding: gzip, deflate, br
+accept-language: pt-BR,pt;q=0.9
+cache-control: max-age=0
+cookie: _octo=GH1.1.380668594.1642420797; _locale=pt-br; _locale_experiment=pt-br; logged_in=no; _device_id=bc8416d3608496c87499987973775fda; _gh_sess=GvzS1Sr%2B5dtgmevDqG%2F1cRuyezacTDaKeMd48n%2BSwBOSSW%2FGYoJ%2F2%2F9tiNpbad9w5YAFv2d0P3%2BVt502ApNw86z96YixhMgYEr218qKt%2BPtTCHjNQyT3bZ5RN581cClbgK6xXxxYznY5zeSVpZIChyw2d3gzOldmXtkeLwt4jm1eKA%2BY33TkCZEw7GjxaPbAUEDWu%2FAeh5yD6WN%2FqPuiME9Ko%2FPzPNfBsOq2p0bOv8O%2B4f5gLzn505pTMeLfyiat5lXsdQ%2FPqJrOhWywKG5%2FHw%3D%3D--v1cKTFnMxegnEuwm--C%2FcK2M1MzNliPlVXLMdDJw%3D%3D; tz=America%2FSao_Paulo
+dnt: 1
+referer: https://github.com/
+sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Windows"
+sec-fetch-dest: document
+sec-fetch-mode: navigate
+sec-fetch-site: same-origin
+sec-fetch-user: ?1
+upgrade-insecure-requests: 1
+
